90c96afd1c60e45e95e066178c4bc67e787b79a0
karli <karliholler@web.de>
2022-12-25 01:08:33 +0100

    Third Commit BottomNavBar

--- app/src/main/java/com/example/sensorapp/Sensors.kt
+++ app/src/main/java/com/example/sensorapp/Sensors.kt
@@ -1,10 +1,17 @@
 package com.example.sensorapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.sensorapp.databinding.ActivityMainBinding
+import com.example.sensorapp.databinding.FragmentSensorsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,20 +28,32 @@ class Sensors : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private val arrayTest = listOf("Test1", "Test2", "Test3", "Test4")
+    private lateinit var binding : FragmentSensorsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        binding = FragmentSensorsBinding.inflate(layoutInflater)
+        val adapter = ArrayAdapter(this.requireContext(), R.layout.list_item, arrayTest)
+        binding.dropdownField.setAdapter(adapter)
+        println("Kontext:" + binding.dropdownField.toString())
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sensors, container, false)
+
+        binding = FragmentSensorsBinding.inflate(layoutInflater)
+        val adapter = ArrayAdapter(this.requireContext(), R.layout.list_item, arrayTest)
+        binding.dropdownField.setAdapter(adapter)
+        //return inflater.inflate(R.layout.fragment_sensors, container, false)
+        return binding.root
     }
 
     companion object {
